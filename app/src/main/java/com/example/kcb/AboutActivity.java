1c05efb9fdc887f02f8396b014e274ab082145cd
Coc888 <2540232967@qq.com>
2020-07-14 13:38:49 +0800

    课程表APP

---
+++ app/src/main/java/com/example/kcb/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.example.kcb;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 关于我们
+ */
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        }
+}
---
+++ app/src/main/java/com/example/kcb/SpinnerActivity.java
@@ -0,0 +1,31 @@
+package com.example.kcb;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+/**
+ * 单双周下拉框选择
+ */
+
+public class SpinnerActivity extends AppCompatActivity {
+    private Spinner mWeek = null;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_course);
+        mWeek = (Spinner) findViewById(R.id.week_choose);
+        String[] arr = {"单周", "双周", "每周"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.week_list, arr);
+        mWeek.setAdapter(adapter);
+        mWeek.setOnItemClickListener((AdapterView.OnItemClickListener)this);
+    }
+}
+
